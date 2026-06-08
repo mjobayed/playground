75c186520037dc6146cc88706895a39826c9088e
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-08 22:28:53 +0600

    feat: create and start a basic fastify server

--- typescript/fastify/fastify-crud/src/server.ts
+++ typescript/fastify/fastify-crud/src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import app from "./app.js";
+
+const PORT = 3000;
+
+const start = async () => {
+  try {
+    console.log(`Server started at http://localhost:${PORT}\n`);
+    app.listen({ port: PORT });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
