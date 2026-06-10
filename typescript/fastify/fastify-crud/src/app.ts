bc6bd9f86d1f9a66b17f21184c87b9d6b18456f2
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-10 16:27:53 +0600

    feat: get and reply with data body

--- typescript/fastify/fastify-crud/src/app.ts
+++ typescript/fastify/fastify-crud/src/app.ts
@@ -1,4 +1,5 @@
 import fastify, { FastifyReply, FastifyRequest } from "fastify";
+import { rootPostBody } from "./types/appTypes.js";
 
 const app = fastify({ logger: true });
 
@@ -6,8 +7,9 @@ app.get("/", async (_, reply: FastifyReply) => {
   reply.code(200).send({ message: "OK" });
 });
 
-app.post("/", async (_request: FastifyRequest, reply: FastifyReply) => {
-  reply.code(201).send({ message: "POST request received" });
+app.post<{ Body: rootPostBody }>("/", async (request, reply: FastifyReply) => {
+  const { name, age } = request.body;
+  reply.code(201).send({ message: "The response", name, age });
 });
 
 export default app;
