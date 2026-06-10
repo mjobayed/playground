e0c93d3701872e382d7de99ea95b125e9997b080
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-10 16:16:19 +0600

    feat: create status check GET request on /

--- typescript/fastify/fastify-crud/src/app.ts
+++ typescript/fastify/fastify-crud/src/app.ts
@@ -1,5 +1,9 @@
-import fastify from "fastify";
+import fastify, { FastifyReply } from "fastify";
 
 const app = fastify({ logger: true });
+
+app.get("/", async (_, reply: FastifyReply) => {
+  reply.code(200).send({ message: "OK" });
+});
 
 export default app;
