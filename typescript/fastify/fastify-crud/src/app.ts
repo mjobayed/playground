e75e37e30e46a52fec97bd246f62798d29f0f3f5
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-10 16:20:30 +0600

    feat: add simple POST request on / route

--- typescript/fastify/fastify-crud/src/app.ts
+++ typescript/fastify/fastify-crud/src/app.ts
@@ -1,9 +1,13 @@
-import fastify, { FastifyReply } from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 
 const app = fastify({ logger: true });
 
 app.get("/", async (_, reply: FastifyReply) => {
   reply.code(200).send({ message: "OK" });
+});
+
+app.post("/", async (_request: FastifyRequest, reply: FastifyReply) => {
+  reply.code(201).send({ message: "POST request received" });
 });
 
 export default app;
