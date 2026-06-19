99937d78ee0b8e2ab0e23696fd59a1030875aa85
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-20 02:26:17 +0600

    feat: create a simple POST endpoint on /

--- typescript/fastify/002-fastify-vitest/src/app.ts
+++ typescript/fastify/002-fastify-vitest/src/app.ts
@@ -6,4 +6,8 @@ app.get("/", async (_, reply: FastifyReply) => {
   reply.code(200).send({ message: "OK" });
 });
 
+app.post("/", async (_, reply) => {
+  reply.code(201).send({ message: "created" });
+});
+
 export default app;
