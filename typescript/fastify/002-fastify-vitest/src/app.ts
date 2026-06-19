d9b19a0b7337d3bea46286e28ce134bbde0a3687
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-20 02:58:09 +0600

    feat(app): disable type coercion with ajv

--- typescript/fastify/002-fastify-vitest/src/app.ts
+++ typescript/fastify/002-fastify-vitest/src/app.ts
@@ -1,6 +1,13 @@
 import fastify, { FastifyReply } from "fastify";
 
-const app = fastify({ logger: true });
+const app = fastify({
+  logger: true,
+  ajv: {
+    customOptions: {
+      coerceTypes: false,
+    },
+  },
+});
 
 app.get("/", async (_, reply: FastifyReply) => {
   reply.code(200).send({ message: "OK" });
