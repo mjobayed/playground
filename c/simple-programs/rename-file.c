faa4b11b6c7c4fa149d0f9abfa07121a3f611a41
Md. Jobayed Islam <contact@mjobayed.com>
2026-08-15 23:02:36 +0600

    feat: check for valid number of arguments

--- c/simple-programs/rename-file.c
+++ c/simple-programs/rename-file.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("Invalid arguments!\n");
+    return 1;
+  }
+
   rename(argv[1], argv[2]);
   printf("File rename successful!\n");
 }
