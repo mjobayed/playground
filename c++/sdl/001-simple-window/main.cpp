7e92e473917a1e3061a2980d1046425d5a8b879e
Md. Jobayed Islam <contact@mjobayed.com>
2026-08-25 20:10:01 +0600

    feat: create renderer
    Still window not showing up. Maybe I need to add
    delay.

--- c++/sdl/001-simple-window/main.cpp
+++ c++/sdl/001-simple-window/main.cpp
@@ -8,8 +8,12 @@ int main() {
   SDL_Window *window =
       SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN);
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
   printf("Running\n");
 
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 }
