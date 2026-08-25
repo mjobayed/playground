aee89c80042556000590c713e67cc11a14c05f73
Md. Jobayed Islam <contact@mjobayed.com>
2026-08-25 20:35:53 +0600

    feat: create and show basic sdl window

---
+++ c++/sdl/002-window-keyboard-event/main.cpp
@@ -0,0 +1,19 @@
+#include <SDL2/SDL.h>
+
+int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window *window =
+      SDL_CreateWindow("Keyboard Event test", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN);
+
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  SDL_RenderPresent(renderer);
+  SDL_Delay(3000);
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
