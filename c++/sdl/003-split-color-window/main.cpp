d9869c6db8e7fe70dffe00ddd726b807a1df7161
Md. Jobayed Islam <contact@mjobayed.com>
2026-08-31 04:38:34 +0600

    feat: create basic window

---
+++ c++/sdl/003-split-color-window/main.cpp
@@ -0,0 +1,29 @@
+#include <SDL2/SDL.h>
+
+int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window =
+      SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       800, 800, SDL_WINDOW_SHOWN);
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+  bool running = true;
+  while (running) {
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
+        running = false;
+      }
+      if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_q) {
+        running = false;
+      }
+    }
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
