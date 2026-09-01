a1a6d57268cfe614001cd3bac40e12f7ca81169c
Md. Jobayed Islam <contact@mjobayed.com>
2026-09-01 10:55:57 +0600

    feat: display text on the window
    It's a 3 step process:
     - create a temporary surface with the text,
     - create a GPU texture with the surface,
     - copy the texture to the renderer.
    
    We of course need to initialize the TTF module and stuff.
    
    Apparently we need to add SDL2_ttf to the Makefile
    flags. And it sdl has to be capitalized for ttf. While,
    sdl2 alone does not need to be capitalized. Like this:
    
    Correct:
    pkg-config --cflags --libs sdl2 SDL2_ttf
    
    Incorrect:
    pkg-config --cflags --libs sdl2 sdl2_ttf
    
    Weird!

--- c++/sdl/004-text-on-screen/main.cpp
+++ c++/sdl/004-text-on-screen/main.cpp
@@ -1,12 +1,22 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
+  TTF_Init();
+
   SDL_Window *window =
       SDL_CreateWindow("Text on Screen", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN);
   SDL_Renderer *renderer =
       SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+  TTF_Font *font = TTF_OpenFont("JetBrainsMono.ttf", 24);
+  SDL_Color textColor = {255, 255, 255, 255};
+  SDL_Surface *textSurface =
+      TTF_RenderText_Solid(font, "Hello, World!", textColor);
+  SDL_Texture *textTexture =
+      SDL_CreateTextureFromSurface(renderer, textSurface);
+  SDL_Rect textRect = {0, 0, 300, 100};
 
   bool running = true;
   while (running) {
@@ -20,10 +30,15 @@ int main() {
       }
     }
 
+    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
     SDL_RenderPresent(renderer);
   }
 
+  SDL_FreeSurface(textSurface);
+  SDL_DestroyTexture(textTexture);
+  TTF_CloseFont(font);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
+  TTF_Quit();
   SDL_Quit();
 }
